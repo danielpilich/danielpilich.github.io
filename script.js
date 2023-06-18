eb7cef06adb6d95318feea21457e81c5e780e4a4
Daniel Pilich <dpilich16@gmail.com>
2023-06-18 18:45:15 +0200

    Upload v1.0 of my portfolio page

---
+++ script.js
@@ -0,0 +1,75 @@
+//Basic Swiper functions
+var swiper = new Swiper(".mySwiper", {
+    hashNavigation: {
+        watchState: true,
+    },
+    direction: "vertical",
+    slidesPerView: 1,
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+    rewind: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//Text and image fade-in
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)        
+        //Single fade-in
+        /*if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }*/
+
+        //Repetitive fade-in
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    });
+});
+document.querySelectorAll('.hidden').forEach((el) => observer.observe(el));
+
+//Typing effect
+let count = 0;
+const observer2 = new IntersectionObserver((events) => {
+    events.forEach((event) => {
+        if (count < 2) {
+            event.target.innerHTML = "";
+            setTimeout(function () {
+                let i = 0;
+                const timer = setInterval(() => {
+                    event.target.innerHTML += event.target.dataset.value.charAt(i);
+                    i++;
+                }, 50);
+            }, event.target.dataset.delay);
+            count++;
+        }
+    });
+});
+document.querySelectorAll(".hack").forEach((elem) => observer2.observe(elem));
+
+//Flying blob moving after the pointer
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    blob.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" });
+}
+
+function showMail() {
+    document.getElementById("mail").textContent = "dpilich16@gmail.com";
+  }
